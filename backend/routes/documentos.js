bb5519c7862226b6f7762a48824276de172e4cb2
Adrian <adriangom0207@gmail.com>
2025-10-16 12:38:19 -0500

    Cambio en documentos.js de require

--- backend/routes/documentos.js
+++ backend/routes/documentos.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const { subirDocumento } = require("../controllers/documentosController");
-const { requireAuth } = require("../middlewares/auth"); // <- usa tu middleware real
+const { requireAuth } = require("../middlewares/authMiddleware");
 
 router.post("/subir", requireAuth, ...subirDocumento);
 
